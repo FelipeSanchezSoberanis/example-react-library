4f62b624a10a2c5a9bde56ccdbeca30b6a59a328
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-09-01 20:04:41 -0600

    feat: more component examples

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import { Button } from "library/components";
+import { TextField, Switch } from "@mui/material";
 
 function App() {
-  return <Button />;
+  return (
+    <div style={{ display: "flex", columnGap: "1rem" }}>
+      <Button />
+      <TextField size="small" focused />
+      <Switch defaultChecked />
+    </div>
+  );
 }
 
 export default App;
