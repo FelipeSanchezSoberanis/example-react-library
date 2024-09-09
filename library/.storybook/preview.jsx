08cce1fa8d75b85dc3acd43d3912441208a682ce
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2024-09-09 09:00:36 -0600

    feat: add path alias, add global theme switcher

--- library/.storybook/preview.jsx
+++ library/.storybook/preview.jsx
@@ -1,4 +1,8 @@
+import { ThemeProvider } from "@mui/material";
 import { Title, Subtitle, Primary, Controls } from "@storybook/blocks";
+import { themeOne, themeTwo, themeThree } from "@themes";
+
+const themesPerBrand = { one: themeOne, two: themeTwo, three: themeThree };
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
@@ -19,6 +23,25 @@ const preview = {
           <Controls />
         </>
       )
+    }
+  },
+  decorators: [
+    (Story, context) => (
+      <ThemeProvider theme={themesPerBrand[context.globals.theme]}>
+        <Story />
+      </ThemeProvider>
+    )
+  ],
+  globalTypes: {
+    theme: {
+      description: "Theme for components",
+      defaultValue: "one",
+      toolbar: {
+        title: "Theme",
+        icon: "circlehollow",
+        items: ["one", "two", "three"],
+        dynamicTitle: true
+      }
     }
   }
 };
